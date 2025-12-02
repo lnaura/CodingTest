82f5e3266eab5c71f63de67094281e6e30bfde0d
jiwon <lnaura@naver.com>
2025-12-02 14:50:27 +0900

    Solve: [BOJ] 11723 / 집합(비트마스킹)

--- Class3/11723_집합.py
+++ Class3/11723_집합.py
@@ -2,31 +2,27 @@ import sys
 input = sys.stdin.readline
 
 m = int(input())
+s = 0  
 
-s = set()
 for _ in range(m):
     command = input().split()
-
-    if len(command) == 1:
-        if command[0] == "all":
-            s = set([str(i) for i in range(1,21)])
-        elif command[0] == "empty":
-            s = set()
-        continue
-
-
-    op, x = command[0], command[1]
-
-    if op == "add":
-        s.add(x) 
-    elif op == "remove":
-        s.discard(x) 
-    elif op == "check":
-        print(1 if x in s else 0)
-    elif op == "toggle":
-        if x in s:
-            s.discard(x)
-        else:
-            s.add(x)
-
+    
+    if command[0] == "all":
+        s = (1 << 21) - 1
+    elif command[0] == "empty":
+        s = 0
+    else:
+        op = command[0]
+        x = int(command[1])
         
+        if op == "add":
+            s |= (1 << x)
+        elif op == "remove":
+            s &= ~(1 << x)
+        elif op == "check":
+            if s & (1 << x):
+                print(1)
+            else:
+                print(0)
+        elif op == "toggle":
+            s ^= (1 << x)
