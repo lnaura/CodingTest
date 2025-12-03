7713352b15b2606f32ac04abdedc27750412a465
jiwon <lnaura@naver.com>
2025-12-03 14:41:54 +0900

    Refactor: [BOJ] 1764 / 듣보잡(Set,sorting)

--- Class3/1764_듣보잡.py
+++ Class3/1764_듣보잡.py
@@ -21,3 +21,19 @@ num = len(heard_see_list)
 print(num)
 for i in range(num):   
     print(heard_see_list[i])
+
+# ---------------------------------
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+heard = set(input().strip() for _ in range(n))
+
+seen = set(input().strip() for _ in range(m))
+
+result = sorted(list(heard & seen))
+
+print(len(result))
+for name in result:
+    print(name)
