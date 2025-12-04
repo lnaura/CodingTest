5c7adf7ad6e76c6a6393ddcf916555e43fea40da
jiwon <lnaura@naver.com>
2025-12-04 15:01:33 +0900

    Refactor: [BOJ] 9375 패션왕 신해빈(Counter)

--- Class3/9375_패션왕신해빈.py
+++ Class3/9375_패션왕신해빈.py
@@ -19,3 +19,25 @@ for _ in range(t):
         result *= (count + 1)
     
     print(result - 1)
+
+#----------------------------
+# Counter 사용하기
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    clothes = []
+    for _ in range(n):
+        a, b = input().split()
+        clothes.append(b)
+    clothes_dict = Counter(clothes)
+
+    result = 1
+    for count in clothes_dict.values():
+        result *= (count + 1)
+    print(result - 1)
