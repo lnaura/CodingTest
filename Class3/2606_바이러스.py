dbc18c076da62cadc109d7f9955adbffe4c6fffe
jiwon <lnaura@naver.com>
2025-12-04 14:03:43 +0900

    Refactor: [BOJ] 2606 / 바이러스(DFS)

--- Class3/2606_바이러스.py
+++ Class3/2606_바이러스.py
@@ -31,3 +31,30 @@ def bfs(graph, start, visited):
 
 print(bfs(networks,1,visited))
 
+#-----------------------------------------
+# DFS
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+computers = int(input())
+n = int(input())
+networks = [[] for _ in range(computers + 1)]
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    networks[a].append(b)
+    networks[b].append(a)
+
+visited = [False] * (computers + 1)
+
+def dfs(v):
+    visited[v] = True
+
+    for i in networks[v]:
+        if not visited[i]:
+            dfs(i)
+
+dfs(1)
+
+print(sum(visited)-1)
