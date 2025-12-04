e73e01791cda211aa99f660fae79151f030402d8
jiwon <lnaura@naver.com>
2025-12-04 14:00:38 +0900

    Solve: [BOJ] 2606 / 바이러스(bfs)

---
+++ Class3/2606_바이러스.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+computers = int(input())
+n = int(input())
+
+networks = [[] for _ in range(computers + 1)]
+for _ in range(n):
+    a,b = map(int,input().split())
+    networks[a].append(b)
+    networks[b].append(a)
+
+visited = [False] * (computers+1)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    cnt = 0
+
+    while queue:
+        v = queue.popleft()
+        cnt += 1
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+    
+    return cnt - 1
+
+print(bfs(networks,1,visited))
+
