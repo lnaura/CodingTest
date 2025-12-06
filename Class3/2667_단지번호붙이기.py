1db891b0912f59e8a171c3566997f8b1805d3a95
jiwon <lnaura@naver.com>
2025-12-06 09:54:47 +0900

    Refactor: [BOJ] 2667 / 단지 번호 붙이기(DFS)

--- Class3/2667_단지번호붙이기.py
+++ Class3/2667_단지번호붙이기.py
@@ -41,3 +41,43 @@ print(cnt - 1)
 houses.sort()
 for house in houses:
     print(house)
+
+#--------------------------------------
+# DFS 풀이
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def dfs(x, y):
+    maps[y][x] = 0
+    
+    count = 1 
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+       
+        if 0 <= nx < n and 0 <= ny < n and maps[ny][nx] == 1:
+            count += dfs(nx, ny)
+            
+    return count
+
+n = int(input())
+maps = [list(map(int, input().strip())) for _ in range(n)]
+
+houses = []
+
+for j in range(n):
+    for i in range(n):
+        if maps[j][i] == 1:
+            house_count = dfs(i, j)
+            houses.append(house_count)
+
+houses.sort()
+
+print(len(houses))
+for h in houses:
+    print(h)
