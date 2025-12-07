b43aff37589d286eb38f0298b6552d7407d8d3b7
jiwon <lnaura@naver.com>
2025-12-07 22:19:59 +0900

    Fix: [BOJ] 17626 / FourSquares (Bruteforce)

--- Class3/17626_FourSquares.py
+++ Class3/17626_FourSquares.py
@@ -20,4 +20,29 @@ for i in range(4,n+1):
         j += 1
 
 print(dp[n])
+
+#-----------------------------------------
+# 브루트포스 + 라그랑주 네 제곱수 정리
+import sys
+input = sys.stdin.readline
+
+def solve(n):
+    if int(n**0.5)**2 == n:
+        return 1
     
+    for i in range(1,int(n**0.5)+1):
+        rem = n - i**2
+        if int(rem**0.5)**2 == rem:
+            return 2
+    
+    for i in range(1, int(n**0.5)+1):
+        rem1 = n - i**2
+        for j in range(1, int(rem1**0.5)+1):
+            rem2 = rem1 - j**2
+            if int(rem2**0.5)**2 == rem2:
+                return 3
+    
+    return 4
+
+n = int(input())
+print(solve(n))
