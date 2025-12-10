26bd53a3bfb73c298b2e50b0ef128e105e9364fa
jiwon <lnaura@naver.com>
2025-12-10 14:59:48 +0900

    Solve: [BOJ] 11724 / 연결요소의 개수(DFS)

---
+++ Class3/11724_연결요소의개수.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m = map(int,input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n+1)
+cnt = 0
+def dfs(graph,v, visited):
+    visited[v] = True
+    for j in graph[v]:
+        if not visited[j]:
+            dfs(graph,j,visited)
+
+for i in range(1,n+1):
+    if not visited[i]:
+        dfs(graph,i,visited)
+        cnt += 1
+print(cnt)
