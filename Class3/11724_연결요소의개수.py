2744fc33b6d6ca119aea018ff69003d85bfe3ed5
jiwon <lnaura@naver.com>
2025-12-10 15:15:14 +0900

    Solve: [BOJ] 11724 / 연결요소의 개수(BFS)

--- Class3/11724_연결요소의개수.py
+++ Class3/11724_연결요소의개수.py
@@ -24,3 +24,38 @@ for i in range(1,n+1):
         dfs(graph,i,visited)
         cnt += 1
 print(cnt)
+
+#---------------------------------------------------
+# BFS
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n+1)
+cnt = 0
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+for i in range(1,n+1):
+    if not visited[i]:
+        bfs(i)
+        cnt += 1
+print(cnt)
