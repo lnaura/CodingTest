e050c1301c3090b3e46b6015169f503d1294b7ea
jiwon <lnaura@naver.com>
2025-12-14 23:46:58 +0900

    Solve: [BOJ] 21736 헌내기는 내친구(BFS)

---
+++ Class3/21736_헌내기는친구가필요해.py
@@ -0,0 +1,40 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+maps = [list(input()) for _ in range(n)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+visited = [[False] * m for _ in range(n)]
+queue = deque()
+cnt = 0
+
+def bfs(y,x):
+    global cnt
+    visited[x][y] = True
+    queue.append((x,y))
+    while queue:
+        cx, cy = queue.popleft()
+        for k in range(4):
+            nx = cx + dx[k]
+            ny = cy + dy[k]
+
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and not maps[nx][ny] == 'X':
+                if maps[nx][ny] == 'P':
+                    cnt += 1
+                visited[nx][ny] = True
+                queue.append((nx,ny))
+
+for j in range(m):
+    for i in range(n):
+        if maps[i][j] == "I":
+            bfs(j,i)
+
+if cnt > 0:
+    print(cnt)
+else:
+    print("TT")
+
+
