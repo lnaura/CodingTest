ac295169d0964a4647153b954d35f663cd682f31
jiwon <lnaura@naver.com>
2025-12-15 17:04:54 +0900

    Refactor: 1389 케빈 베이컨의 6단계법칙 (플로이드 워셜)

--- Class3/1389_케빈베이컨의6단계법칙.py
+++ Class3/1389_케빈베이컨의6단계법칙.py
@@ -32,3 +32,32 @@ for i in range(1,n+1):
         min_i = i
 
 print(min_i)
+
+#------------------------------------------------
+# 플로이드 워셜 알고리즘 풀이
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+
+n, m = map(int, input().split())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a][b] = graph[b][a] = 1
+
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b],graph[a][k] + graph[k][b])
+
+min_num = INF
+for i in range(1,n+1):
+    num = sum(graph[i][1:]) 
+    if min_num > num :
+        min_num = num
+        min_i = i
+print(min_i)
+
