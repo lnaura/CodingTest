357a9f8e5c68d43524a4e0e5ab2a85ddd8b22297
jiwon <lnaura@naver.com>
2025-12-23 15:45:27 +0900

    Solve: [BOJ] 11725 트리의 부모 찾기(dfs)

--- Class4/11725_트리의부모찾기.py
+++ Class4/11725_트리의부모찾기.py
@@ -27,3 +27,28 @@ bfs(1)
 for i in range(2,n+1):
     print(parents_node[i])
     
+#-------------------------------------------
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+parents_node = [0] * (n+1)
+
+def dfs(x):
+    for nx in graph[x]:
+        if not parents_node[nx]:
+            parents_node[nx] = x
+            dfs(nx)
+
+parents_node[1] = 1
+dfs(1)
+for i in range(2,n+1):
+    print(parents_node[i])
