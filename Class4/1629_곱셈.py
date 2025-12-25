d334469d7c6fec80d522b5cd828a71e1bcc2c9e0
jiwon <lnaura@naver.com>
2025-12-25 14:10:59 +0900

    Solve: [BOJ] 1629 곱셈(분할과정복)

---
+++ Class4/1629_곱셈.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+a,b,c = map(int,input().split())
+
+result = a
+def solve(a,b,c):
+    if b == 1:
+        return a % c
+    
+    temp = solve(a,b//2,c)
+    
+    if b % 2 == 0:
+        return (temp * temp) % c
+    else:
+        return (temp * temp * a) % c
+
+print(solve(a,b,c))
