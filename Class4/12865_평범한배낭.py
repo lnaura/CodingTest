505fcbe8720066d975e035e5bf2b4c1af16a5693
jiwon <lnaura@naver.com>
2025-12-29 17:05:48 +0900

    Refactor: [BOJ] 12865 평범한 배낭(dp)

--- Class4/12865_평범한배낭.py
+++ Class4/12865_평범한배낭.py
@@ -20,3 +20,19 @@ for i in range(1,n+1):
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
 
 print(dp[n][k])
+
+#-------------------------------------------
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+dp = [0] * (k + 1)
+
+for _ in range(n):
+    w,v = map(int,input().split())
+    
+    for j in range(k,w-1,-1):
+        dp[j] = max(dp[j],dp[j-w]+ v)
+        
+print(dp[k])
