e17336ce4e1dbd6e74b2cffdaab51befca8d18bb
jiwon <lnaura@naver.com>
2026-01-07 13:50:42 +0900

    Refactor: [BOJ] 14938 서강그라운드(플로이드워셜)

--- Class4/14938_서강그라운드.py
+++ Class4/14938_서강그라운드.py
@@ -46,3 +46,38 @@ for i in range(1,n+1):
     result = max(result,temp_sum)
     
 print(result)
+
+#---------------------------------------------
+# 플로이드 워셜
+import sys
+input = sys.stdin.readline
+
+INF = sys.maxsize
+
+n , m , r = map(int,input().split())
+
+items = [0] + list(map(int,input().split()))
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    graph[i][i] = 0
+    
+for _ in range(r):
+    a,b,l = map(int,input().split())
+    graph[a][b] = l
+    graph[b][a] = l
+
+for k in range(1,n+1):
+    for i in range(1, n+1):
+        for j in range(1,n+1):
+            graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
+
+result = 0
+for i in range(1,n+1):
+    item_cnt = 0
+    for j in range(1,n+1):
+        if graph[i][j] <= m:
+            item_cnt += items[j]
+    result = max(result,item_cnt)
+    
+print(result)
