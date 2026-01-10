42423c07944d3426f8b7d0e4988db88b5ffdafac
jiwon <lnaura@naver.com>
2026-01-10 15:35:38 +0900

    Solve [BOJ] 30805 사전 순 최대 공통 부분 수열(Set)

--- Class4/30805_사전순최대공통부분수열.py
+++ Class4/30805_사전순최대공통부분수열.py
@@ -34,3 +34,37 @@ while True:
 
 print(len(result))
 print(*result)
+
+#-------------------------------------------------------
+# set 활용.
+import sys
+input = sys.stdin.readline
+
+a_len = int(input())
+a = list(map(int,input().split()))
+
+b_len = int(input())
+b = list(map(int,input().split()))
+
+max_a_index = -1
+max_b_index = -1
+sub_a = []
+sub_B = []
+result = []
+while True:
+    sub_a = a[max_a_index+1 :]
+    sub_b = b[max_b_index+1 :]
+    
+    common_set = set(sub_a) & set(sub_b)
+    
+    if not common_set:
+        break
+    
+    max_val = max(common_set)
+    result.append(max_val)
+    
+    max_a_index += 1 + sub_a.index(max_val)
+    max_b_index += 1 + sub_b.index(max_val)
+    
+print(len(result))
+print(*result)
